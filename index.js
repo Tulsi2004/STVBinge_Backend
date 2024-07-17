67cbc7602b930f5b156af68208af530d43168391
Tulsi2004 <138577151+Tulsi2004@users.noreply.github.com>
2024-07-17 13:51:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 8000;
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 require('./db')
 
 app.use(bodyParser.json());
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000'];
 
-app.get('/',(req, res) => {
-    res.json({
-        message: 'The API is working'
-    });
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true, // Allow credentials
+    })
+);
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'The API is working' });
 });
 
 app.listen(PORT, () => {
